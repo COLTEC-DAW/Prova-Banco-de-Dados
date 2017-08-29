aa4d3042ae3795032891e9e0fbbb897ec106d4b1
Gustavo de Paula <gustavo@depaula.cz>
2017-08-29 11:35:14 -0300

    Fix 2

--- _gustavo-depaula/modelorelacional/create.sql
+++ _gustavo-depaula/modelorelacional/create.sql
@@ -5,13 +5,13 @@ use `daw-aluno08-2017`;
 
 create table impressoras (
 	id int auto_increment not null primary key,
-    nome varchar(100) not null,
+	nome varchar(100) not null,
 	marca varchar(100) not null 
 );
 
 create table clientes (
 	id int auto_increment not null primary key,
-    nome varchar(100) not null,
+	nome varchar(100) not null,
 	cpf varchar(11) not null
 );
 
@@ -23,23 +23,23 @@ create table funcionarios (
 
 create table cargos (
 	id int auto_increment not null primary key,
-    funcionario_id int not null,
-    nome varchar(100) not null,
+	funcionario_id int not null,
+	nome varchar(100) not null,
 	salario decimal(10,2) not null,
-    
-    foreign key (funcionario_id) references funcionarios(id)
+	
+	foreign key (funcionario_id) references funcionarios(id)
 );
 
 create table atendimentos (
 	id int auto_increment not null primary key,
-    impressora_id int not null,
-    cliente_id int not null,
-    funcionario_id int not null,
-    num_paginas int not null,
-    valor decimal(10,2) not null,
-    quando timestamp,
-    
-    foreign key (impressora_id) references impressoras(id),
-    foreign key (cliente_id) references clientes(id),
-    foreign key (funcionario_id) references funcionarios(id)
+	impressora_id int not null,
+	cliente_id int not null,
+	funcionario_id int not null,
+	num_paginas int not null,
+	valor decimal(10,2) not null,
+	quando timestamp,
+	
+	foreign key (impressora_id) references impressoras(id),
+	foreign key (cliente_id) references clientes(id),
+	foreign key (funcionario_id) references funcionarios(id)
 );
